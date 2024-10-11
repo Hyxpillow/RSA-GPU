1885e4d0f9988e2fe8aaf1a96dff1764fb140808
yipeng0016 <62392338+yipeng0016@users.noreply.github.com>
2024-10-11 23:18:52 +0800

    implement mod (#6)

--- cpu/big_num.h
+++ cpu/big_num.h
@@ -8,6 +8,11 @@ struct BigNum {
     BigNum();
     BigNum(unsigned char x);
     BigNum(const BigNum& x);
+    BigNum(const std::string &numStr);
+
+    void convertFromDecimalString(const std::string &numStr);
+
+    BigNum& operator=(const BigNum &other);
 };
 
 
@@ -15,3 +20,11 @@ struct BigNum {
 void multiply(const BigNum& num1,
                 const BigNum& num2,
                 BigNum& result);
+
+void mod(const BigNum& Dividend,
+            const BigNum& Divisor,
+            BigNum& remainder);
+
+void subtract(const BigNum& a, const BigNum& b, BigNum& result);
+void shiftRight(BigNum &num);
+int compare(const BigNum& a, const BigNum& b);
