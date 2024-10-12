a9db68cd775876162380c9a38c1c78e724388d7a
yipeng0016 <478645355@qq.com>
2024-10-12 16:33:58 -0400

    fixbug mod error

--- cpu/big_num.h
+++ cpu/big_num.h
@@ -26,5 +26,5 @@ void mod(const BigNum& Dividend,
             BigNum& remainder);
 
 void subtract(const BigNum& a, const BigNum& b, BigNum& result);
-void shiftRight(BigNum &num);
+void shiftLeft(BigNum &num);
 int compare(const BigNum& a, const BigNum& b);
