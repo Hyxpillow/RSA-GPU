fea2a83a81329f23d7f894301f3a95cbd7b55264
Hyxpillow <hyxpillow@10-17-236-49.dynapool.wireless.nyu.edu>
2024-10-18 12:41:36 -0400

    load&save

--- utils/pkcs1/pkcs1.h
+++ utils/pkcs1/pkcs1.h
@@ -5,5 +5,20 @@
 #include <string>
 #include <vector>
 
-std::vector<BigNum> load_and_pad_file(size_t module_length, const std::string& filename, unsigned char flag);
-std::vector<unsigned char> pkcs1_padding(const std::vector<unsigned char>& data, size_t module_length, unsigned char flag);
+void save_not_pad_file(size_t modulus_length,
+                       const std::string &filename,
+                       const std::vector<BigNum> &output_blocks);
+
+void save_pad_file(size_t modulus_length,
+                   const std::string &filename,
+                   unsigned char flag,
+                   const std::vector<BigNum> &output_blocks);
+
+std::vector<BigNum> load_and_not_pad_file(size_t modulus_length,
+                                          const std::string &filename);
+std::vector<BigNum> load_and_pad_file(size_t modulus_length,
+                                      const std::string &filename,
+                                      unsigned char flag);
+std::vector<unsigned char> pkcs1_padding(const std::vector<unsigned char> &data,
+                                         size_t modulus_length,
+                                         unsigned char flag);
