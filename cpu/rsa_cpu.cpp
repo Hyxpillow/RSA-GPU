9bfc6e310f5789c6c519a78adf5e695d9dc4290f
yipeng0016 <478645355@qq.com>
2024-11-10 15:51:34 +0000

    enable Montgomery

--- cpu/rsa_cpu.cpp
+++ cpu/rsa_cpu.cpp
@@ -1,4 +1,5 @@
 #include "rsa_cpu.h"
+#include "cpu-modexp/modexp.h"
 #include <vector>
 #include <openssl/bn.h>
 
@@ -12,8 +13,10 @@ void rsa_cpu(const std::vector<BIGNUM*>& input_blocks,
         output_blocks.push_back(output_block_i);
     }
     for (int i = 0; i < input_blocks.size(); i++) {
-        BN_CTX *ctx = BN_CTX_new();
-        BN_mod_exp(output_blocks[i], input_blocks[i], exponent, modulus, ctx);
-        BN_CTX_free(ctx);
+        // BN_CTX *ctx = BN_CTX_new();
+        // BN_mod_exp(output_blocks[i], input_blocks[i], exponent, modulus, ctx);
+        // BN_CTX_free(ctx);
+        MontgomeryModExp montgomery_mod_exp(modulus);
+        montgomery_mod_exp.mont_exp(input_blocks[i], exponent, output_blocks[i]);
     }
 }
