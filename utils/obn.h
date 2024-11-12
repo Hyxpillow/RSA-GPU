05ca53b7315d791d435b24fd34c062199af73362
hyxpillow <hyxpillow@outlook.com>
2024-11-12 17:20:17 +0000

    impl obn

--- utils/obn.h
+++ utils/obn.h
@@ -15,14 +15,14 @@ void OBN_mul(OURBIGNUM *r, const OURBIGNUM *a, const OURBIGNUM *b);
 void OBN_div(OURBIGNUM *dv, OURBIGNUM *rem, const OURBIGNUM *m, const OURBIGNUM *d);
 # define OBN_mod(rem,m,d,ctx) OBN_div(NULL,(rem),(m),(d),(ctx))
 
+int OBN_lshift(OURBIGNUM *r, const OURBIGNUM *a, int n);
+int OBN_rshift(OURBIGNUM *r, const OURBIGNUM *a, int n);
+
 int OBN_cmp(const OURBIGNUM *a, const OURBIGNUM *b);
 int OBN_is_zero(const OURBIGNUM *a);
-void OBN_add_byte(OURBIGNUM *a, const unsigned char b);
 
-void OBN_obn2bn(BIGNUM* a, const OURBIGNUM *b);
+void OBN_obn2bn(BIGNUM** a, const OURBIGNUM *b);
 void OBN_bn2obn(OURBIGNUM *a, const BIGNUM* b);
 
-int OBN_lshift(BIGNUM *r, const BIGNUM *a, int n);
-int OBN_rshift(BIGNUM *r, const BIGNUM *a, int n);
-
-void OBN_set_byte(OURBIGNUM *a, unsigned char b);
+void OBN_obn2hex(const OURBIGNUM *a, char *str);
+void OBN_hex2obn(OURBIGNUM *a, const char *str);
